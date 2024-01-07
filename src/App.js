8820f2fc1629af38677bdc939f36a8702c943009
sougata <sougata19582001@gmail.com>
2024-01-07 19:52:50 +0530

    added sorting functionality

--- src/App.js
+++ src/App.js
@@ -61,10 +61,28 @@ function Item({ item, handleDeleteItem, handleToggleItem }) {
 }
 
 function PackingList({ items, handleDeleteItem, handleToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItem;
+  if (sortBy === "input") {
+    sortedItem = items;
+  }
+
+  if (sortBy === "description") {
+    sortedItem = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  }
+
+  if (sortBy === "packed") {
+    sortedItem = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItem.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -73,6 +91,13 @@ function PackingList({ items, handleDeleteItem, handleToggleItem }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
